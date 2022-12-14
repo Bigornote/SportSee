6d821412b443111c24e0758d3a74901cb6bed6e3
Antoine Buard <a.buard@laposte.net>
2022-12-14 14:29:55 +0100

    Github change api en API

--- front-end/src/components/Charts/ResultChart.js
+++ front-end/src/components/Charts/ResultChart.js
@@ -33,7 +33,7 @@ const ResultChart = ({ data }) => {
           data={score}
           innerRadius="70%"
           startAngle={90}
-          endAngle={450} // (360 + 90)
+          endAngle={450}
           barSize={10}
         >
           <PolarAngleAxis type="number" domain={[0, 100]} tick={false} />
